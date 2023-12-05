ff8ceafbff5ab7a55de5923df69e55215dc00374
Robert McConnell <robertmcconnell@Roberts-Mac-mini.local>
2023-12-05 06:33:12 -0700

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {puppyList} from './data.js'
 
-function App() {
- 
 
-  return (
-    <>
-      <div>
+function App () {
+  //console.log(puppyList)
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log("puppyList: ", puppyList)
+    return (
+      <>
+        <div>
+        {puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>
+        })
+        }
+
         
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+       </div>
+      
+     </>
   )
 }
 
